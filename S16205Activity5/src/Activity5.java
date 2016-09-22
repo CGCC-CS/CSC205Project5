6aeb024210de3793eecda4da4fc87fcc95ee265a
WADGV56140 <WADGV56140@C104-INST.cgc.maricopa.edu>
2016-09-21 20:55:07 -0700

    F16 Fixes

---
+++ S16205Activity5/src/Activity5.java
@@ -0,0 +1,17 @@
+import cards.*;
+import hands.*;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		Deck d = new Deck(52);
+		HandOfCards hand = new HandOfTwo();
+		
+		hand.addCard(d.dealOne());
+		hand.addCard(d.dealOne());
+
+		System.out.println("Hand :");
+		hand.printHand();
+	}
+	
+}
