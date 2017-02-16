ec2ff1c5a65abd68399ea2d306466938f412f209
Wade Huber <wadehuber@gmail.com>
2017-02-15 20:54:59 -0700

    Fixed ArrayHand toString to not print null cards.

--- Activity5Code/src/Activity5.java
+++ Activity5Code/src/Activity5.java
@@ -5,8 +5,10 @@ public class Activity5 {
 
 	public static void main(String[] args) {
 		Deck d = new Deck(52);
-		HandOfCards hand = new HandOfTwo();
+		HandOfCards hand = new ArrayHand(5);
 		
+		hand.addCard(d.dealOne());
+		hand.addCard(d.dealOne());
 		hand.addCard(d.dealOne());
 		hand.addCard(d.dealOne());
 
