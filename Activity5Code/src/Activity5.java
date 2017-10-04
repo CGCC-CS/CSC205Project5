4176c94333dd4c5cceaa09f98a9b4f0ba32254a9
wadgv56140 <wadgv56140@C104-INST.cgc.maricopa.edu>
2017-10-03 21:01:00 -0700

    Removed default size.

--- Activity5Code/src/Activity5.java
+++ Activity5Code/src/Activity5.java
@@ -5,7 +5,7 @@ public class Activity5 {
 
 	public static void main(String[] args) {
 		Deck d = new Deck(52);
-		HandOfCards hand = new HandOfTwo();
+		HandOfCards hand = new ArrayHand();
 		
 		hand.addCard(d.dealOne());
 		hand.addCard(d.dealOne());
