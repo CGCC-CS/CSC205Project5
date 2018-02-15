eb6aa2402db039b621f55773cb50d6b3cb22feb1
wadgv56140 <wadgv56140@C104-INST.cgc.maricopa.edu>
2018-02-14 21:13:51 -0700

    Added implements

--- Activity5Code/src/hands/ArrayListHand.java
+++ Activity5Code/src/hands/ArrayListHand.java
@@ -1,7 +1,9 @@
 package hands;
 
+import java.util.Iterator;
+
 // Complete ArrayListHands which implements the HandOfCards interface using an ArrayList
 
-public class ArrayListHand implements Iterable {
+public class ArrayListHand implements HandOfCards, Iterable {
 
 }
